9daf80fa97e506c68f01847249027e46f2dda0d2
amr <amroali.ca@gmail.com>
2020-11-03 19:19:54 -0500

    aggregate

--- src/seed.ts
+++ src/seed.ts
@@ -1,25 +1,17 @@
-
-
-
 import { PrismaClient } from '@prisma/client'
-import { add, max } from 'date-fns'
+import { add } from 'date-fns'
 
-const prisma = new PrismaClient({
-  log: ['error', 'warn'],
-})
+// Instantiate Prisma Client
+const prisma = new PrismaClient()
 
 // A `main` function so that we can use async/await
 async function main() {
-  // Seed the database with users and posts
-  await prisma.courseEnrollment.deleteMany({})
+  // await prisma.token.deleteMany({})
   await prisma.testResult.deleteMany({})
-  await prisma.user.deleteMany({})
+  await prisma.courseEnrollment.deleteMany({})
   await prisma.test.deleteMany({})
+  await prisma.user.deleteMany({})
   await prisma.course.deleteMany({})
-
-  const weekFromNow = add(new Date(), { days: 7 })
-  const twoWeekFromNow = add(new Date(), { days: 14 })
-  const monthFromNow = add(new Date(), { days: 28 })
 
   const grace = await prisma.user.create({
     data: {
@@ -32,46 +24,154 @@ async function main() {
       },
     },
   })
-  console.log(grace)
+
+  const weekFromNow = add(new Date(), { days: 7 })
+  const twoWeekFromNow = add(new Date(), { days: 14 })
+  const monthFromNow = add(new Date(), { days: 28 })
 
   const course = await prisma.course.create({
-  data: {
-    name: 'CRUD with Prisma',
-    tests: {
-      create: [
-        {
-          date: weekFromNow,
-          name: 'First test',
-        },
-        {
-          date: twoWeekFromNow,
-          name: 'Second test',
-        },
-        {
-          date: monthFromNow,
-          name: 'Final exam',
-        },
-      ],
-    },
-    members: {
-      create: {
-        role: 'TEACHER',
-        user: {
-          connect: {
-            email: grace.email,
+    data: {
+      name: 'CRUD with Prisma',
+      tests: {
+        create: [
+          {
+            date: weekFromNow,
+            name: 'First test',
+          },
+          {
+            date: twoWeekFromNow,
+            name: 'Second test',
+          },
+          {
+            date: monthFromNow,
+            name: 'Final exam',
+          },
+        ],
+      },
+      members: {
+        create: {
+          role: 'TEACHER',
+          user: {
+            connect: {
+              email: grace.email,
+            },
           },
         },
       },
     },
-  },
-  include: {
-    tests: true,
-    members: {
-      include: {user: true},
-    }
-  },
-})
-console.log(course)
+    include: {
+      tests: true,
+    },
+  })
+
+  const shakuntala = await prisma.user.create({
+    data: {
+      email: 'devi@prisma.io',
+      firstName: 'Shakuntala',
+      lastName: 'Devi',
+      courses: {
+        create: {
+          role: 'STUDENT',
+          course: {
+            connect: { id: course.id},
+          },
+        },
+      },
+    },
+  })
+
+  const david = await prisma.user.create({
+    data: {
+      email: 'david@prisma.io',
+      firstName: 'David',
+      lastName: 'Deutsch',
+      courses: {
+        create: {
+          role: 'STUDENT',
+          course: {
+            connect: { id: course.id },
+          },
+        },
+      },
+    },
+  })
+
+  const testResultsDavid = [650, 900, 950]
+  const testResultsShakuntala = [800, 950, 910]
+
+  let counter = 0
+  for (const test of course.tests) {
+    await prisma.testResult.create({
+      data: {
+        gradedBy: {
+          connect: { email: grace.email },
+        },
+        student: {
+          connect: { email: shakuntala.email },
+        },
+        test: {
+          connect: { id: test.id },
+        },
+        result: testResultsShakuntala[counter],
+      },
+    })
+
+    await prisma.testResult.create({
+      data: {
+        gradedBy: {
+          connect: { email: grace.email },
+        },
+        student: {
+          connect: { email: david.email },
+        },
+        test: {
+          connect: { id: test.id },
+        },
+        result: testResultsDavid[counter],
+      },
+    })
+
+    // Get aggregates for each test
+    const results = await prisma.testResult.aggregate({
+      where: {
+        testId: test.id,
+      },
+      avg: { result: true },
+      max: { result: true },
+      min: { result: true },
+      count: true,
+    })
+    console.log(`test: ${test.name} (id: ${test.id})`, results)
+
+    counter++
+  }
+
+  // Get aggregates for David
+  const davidAggregates = await prisma.testResult.aggregate({
+    where: {
+      student: { email: david.email },
+    },
+    avg: { result: true },
+    max: { result: true },
+    min: { result: true },
+    count: true,
+  })
+  console.log(`David's results (email: ${david.email})`, davidAggregates)
+
+  // Get aggregates for Shakuntala
+  const shakuntalaAggregates = await prisma.testResult.aggregate({
+    where: {
+      student: { email: shakuntala.email },
+    },
+    avg: { result: true },
+    max: { result: true },
+    min: { result: true },
+    count: true,
+  })
+  console.log(
+    `Shakuntala's results (email: ${shakuntala.email})`,
+    shakuntalaAggregates,
+  )
 }
 
 main()
@@ -80,5 +180,25 @@ main()
     process.exit(1)
   })
   .finally(async () => {
+    // Disconnect Prisma Client
     await prisma.$disconnect()
   })
+
+
+
+//   Adding a single test result would look as follows:
+
+// await prisma.testResult.create({
+//   data: {
+//     gradedBy: {
+//       connect: { email: grace.email },
+//     },
+//     student: {
+//       connect: { email: shakuntala.email },
+//     },
+//     test: {
+//       connect: { id: test.id },
+//     },
+//     result: 950,
+//   },
+// })
